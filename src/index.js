9f7fd4015127e52fe83da7419019ff244cdf43df
Whasajiggawho <Whoosajiggawha@gmail.com>
2020-04-28 15:29:32 -0400

    Typo

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ let quoteDiv= document.getElementById('quote')
 fetch('https://api.kanye.rest')
 	.then(res => res.json())
 	.then(quote => {
-		quotesDiv.innerHtml += '<p> ${quote.quote} </p>'
+		quoteDiv.innerHtml += '<p> ${quote.quote} </p>'
 	})
