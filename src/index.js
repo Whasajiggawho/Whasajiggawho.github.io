289ce73a49a97a3426eeea9f23933f9f371e73e9
Whasajiggawho <Whoosajiggawha@gmail.com>
2020-04-28 15:31:53 -0400

    test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-let quoteDiv= document.getElementById('quote')
+let quoteDiv=document.getElementById('quote')
+quoteDiv.innerHtml += `<p>test</p>`
 fetch('https://api.kanye.rest')
-	.then(res => res.json())
-	.then(quote => {
-		quoteDiv.innerHtml += '<p> ${quote.quote} </p>'
-	})
+.then(res => res.json())
+.then(quote => {
+	quoteDiv.innerHtml += `<p> ${quote.quote} </p>`
+})
